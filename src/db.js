77ad64b291cd51d127ae439c4f1d1c13c867b80b
Alex Korotkov <programmerweb@yandex.ru>
2018-03-01 00:49:17 +0300

    Added a Procfile.

--- src/db.js
+++ src/db.js
@@ -1,9 +1,11 @@
 import mysql from 'mysql';
 
 export const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'mydb'
+  host: 'pri114.cmhwdist1pdi.us-east-1.rds.amazonaws.com',
+  port: '3306',
+  user: 'pri114',
+  password: 'bbvjw;g6Sc',
+  database: 'pri114'
 });
 
 connection.connect();
