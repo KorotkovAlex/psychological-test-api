2c4c1b57257d216753cf77bef01427dcb342f552
Alex Korotkov <programmerweb@yandex.ru>
2018-03-01 00:52:37 +0300

    Added a Procfile.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
 import { app } from './app';
 
-app.listen(process.env.PORT || 3000);
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
