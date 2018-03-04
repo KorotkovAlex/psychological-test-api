cb87fb4fd3a0c71a1886d1b4342890034738a92c
Vlad <dv.vlad98@mail.ru>
2018-03-04 18:35:30 +0300

    Add in response id_answer. Add id_answer in order by.

--- src/components/questions/index.js
+++ src/components/questions/index.js
@@ -4,7 +4,7 @@ import { connection } from '../../db';
 const app = express();
 
 app.post('/id', (req, res) => {
-  connection.query(`SELECT questions.id_question, questions.description, answers.title, answers.weight, answers.index_answer, qha.index_question from questions, questions_has_answers as qha, answers where id_test = ${req.body.id_test} and qha.id_question = questions.id_question and qha.id_answer = answers.id_answer ORDER BY questions.id_question, questions.description, qha.index_question, answers.title, answers.weight, answers.index_answer`, (error, results) => {
+  connection.query(`SELECT questions.id_question, questions.description, answers.id_answer, answers.title, answers.weight, answers.index_answer, qha.index_question from questions, questions_has_answers as qha, answers where id_test = ${req.body.id_test} and qha.id_question = questions.id_question and qha.id_answer = answers.id_answer ORDER BY questions.id_question, questions.description, qha.index_question, answers.index_answer, answer.id_answer, answers.title, answers.weight`, (error, results) => {
     if (error) {
       throw error;
     }
