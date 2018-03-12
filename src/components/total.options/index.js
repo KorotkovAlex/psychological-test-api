75728c535d60023d4046c1e8acd5b0260ed5f392
Alex Korotkov <programmerweb@yandex.ru>
2018-03-12 10:22:42 +0300

    change from id_tests to id_test

--- src/components/total.options/index.js
+++ src/components/total.options/index.js
@@ -4,17 +4,17 @@ import { connection } from '../../db';
 const app = express();
 
 app.post('/', (req, res) => {
-    const { id_tests } = req.body;
+  const { id_test } = req.body;
 
-    connection.query(`select from_values, to_values, description, title from total_options where id_tests=${id_tests}`, (error, results) => {
-        if (error) {
-            throw error;
-        }
+  connection.query(`select from_values, to_values, description, title from total_options where id_tests=${id_test}`, (error, results) => {
+    if (error) {
+      throw error;
+    }
 
-        res.json({
-            total_options: results
-        });
+    res.json({
+      total_options: results
     });
+  });
 });
 
 export default app;
