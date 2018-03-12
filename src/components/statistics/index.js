3896923c154a6fbff0e1d495b4dc823e35b1c832
Alex Korotkov <programmerweb@yandex.ru>
2018-03-12 23:58:02 +0300

    sort date desc

--- src/components/statistics/index.js
+++ src/components/statistics/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.post('/own', (req, res) => {
   const { id_test, mail } = req.body;
 
-  connection.query(`select statistics.date, statistics.count_time, statistics.number_point, ts.title as tstitle, ts.description, tests.title from statistics, total_options as ts, tests where statistics.id_test=${id_test} and statistics.mail='${mail}' and ts.id_tests = ${id_test} and  tests.id_tests = ${id_test} and ts.from_values <= statistics.number_point and ts.to_values >= statistics.number_point`, (error, results) => {
+  connection.query(`select statistics.date, statistics.count_time, statistics.number_point, ts.title as tstitle, ts.description, tests.title from statistics, total_options as ts, tests where statistics.id_test=${id_test} and statistics.mail='${mail}' and ts.id_tests = ${id_test} and  tests.id_tests = ${id_test} and ts.from_values <= statistics.number_point and ts.to_values >= statistics.number_point ORDER BY statistics.date DESC, statistics.count_time, statistics.number_point, tstitle, ts.description, tests.title`, (error, results) => {
     res.json({
       statistics: results
     });
--- src/components/statistics/statistics-test.js
+++ src/components/statistics/statistics-test.js
@@ -3,36 +3,25 @@ import { app } from '../../app';
 
 
 describe('request to api', () => {
-  test('returns users', () => {
+  test('returns statistics by id', () => {
     const statistics = {
-      id_test: 'dasas',
-      mail: 'programmerweb@yandex.ru'
+      statistics: [{
+        date: '2018-02-14T21:00:00.000Z',
+        id_statistics: 1,
+        id_test: 1,
+        mail: 'dv@mail.ru',
+        number_point: 51
+      }]
     };
 
     return request(app)
       .post('/statistics/own')
-      .send(statistics)
-      .then(response => {
-        expect(response.body).toEqual({});
+      .send({
+        id_test: 2,
+        mail: 'ka@'
       })
+      .then(response => {
+        expect(response.body).toEqual(statistics);
+      });
   });
-  //
-  // test('returns statistics by id', () => {
-  //   const statistics = {
-  //     statistics: [{
-  //       date: '2018-02-14T21:00:00.000Z',
-  //       id_statistics: 1,
-  //       id_test: 1,
-  //       mail: 'dv@mail.ru',
-  //       number_point: 51
-  //     }]
-  //   };
-  //
-  //   return request(app)
-  //     .post('/statistics/id')
-  //     .send({ id_statistics: 1 })
-  //     .then(response => {
-  //       expect(response.body).toEqual(statistics);
-  //     });
-  // });
 });
