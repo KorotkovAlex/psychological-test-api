0a3839f27d74d8fac33fe5279c9b982d9f279c27
Alex Korotkov <programmerweb@yandex.ru>
2018-03-26 13:39:09 +0300

    change host

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql';
 
 export const connection = mysql.createConnection({
-  host: 'pri114.cmhwdist1pdi.us-east-1.rds.amazonaws.com',
+  host: 'localhost',
   port: '3306',
   user: 'pri114',
   password: 'bbvjw;g6Sc',
